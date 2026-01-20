36a96488d4decf8080779a26abe5f5e1b0d67d66
Michelle Raouf <michelle.raouf@52ndsolution.net>
2026-01-20 11:56:04 +0200

    build: Bump version to 1.4.2
    - Update the library version from 1.4.1 to 1.4.2 in `build.gradle.kts`.

--- KLocalNotification/build.gradle.kts
+++ KLocalNotification/build.gradle.kts
@@ -40,7 +40,7 @@ tasks.withType<PublishToMavenRepository> {
 extra["packageNameSpace"] = "io.tbib.klocal_notification"
 extra["groupId"] = "io.github.the-best-is-best"
 extra["artifactId"] = "klocal-notification"
-extra["version"] = "1.4.1"
+extra["version"] = "1.4.2"
 extra["packageName"] = "KLocalNotification"
 extra["packageUrl"] = "https://github.com/the-best-is-best/KLocalNotification"
 extra["packageDescription"] =
